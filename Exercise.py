5ab17431ba24d6b087ff6fef245a079cf26f31d9
Mathew Calkins <mathewpcalkins@gmail.com>
2018-06-30 18:33:47 -0400

    removed solution

--- Exercise.py
+++ Exercise.py
@@ -1,15 +1,4 @@
 import numpy as np
 
 def reward(R, gamma):
-	sum = 0.0
-	for i in range(400):
-#		print(sum)
-		sum += gamma**i
-	return R*sum
-
-if __name__ == "__main__":
-	print("hello world")
-	x = reward(0.5, 10)
-	print(x)
-
-
+	return -1.0
